cb907fc71f1015bf27bf144a213f0cfbec7c682a
Mitchell Lev Merka <lev@merka.us>
2023-02-06 18:21:57 -0600

    default animate install

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Mitchell Lev Merka's Portfolio
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://deathchamber.life"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Download my CV
         </a>
       </header>
     </div>
